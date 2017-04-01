b106e18e8e32a414cf526815638aee623f74e0b5
Majkel44 <m.werner9205@gmail.com>
2017-04-01 14:02:10 +0200

    - fourth release

---
+++ Debug/RGBLight.cpp.d
@@ -0,0 +1,18 @@
+C:\Users\Mike\AppData\Local\Temp\VMicroBuilds\RGBLight\nano_atmega328\RGBLight.cpp.o: \
+ C:\Users\Mike\AppData\Local\Temp\VMicroBuilds\RGBLight\nano_atmega328\RGBLight.cpp \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/arduino.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/binary.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WCharacter.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/WString.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/HardwareSerial.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Print.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Printable.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\eightanaloginputs/pins_arduino.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\eightanaloginputs/../standard/pins_arduino.h \
+ C:\Users\Mike\Documents\Arduino\libraries\OneWire-master/OneWire.h \
+ E:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ C:\Users\Mike\Documents\Arduino\libraries\Arduino-Temperature-Control-Library-master/DallasTemperature.h \
+ C:\Users\Mike\AppData\Local\Temp\VMicroBuilds\RGBLight\nano_atmega328\SuperfluxController.h
