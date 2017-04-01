05fd3235ae7460043b9a8c19a5cfe2117f8745f3
Majkel44 <m.werner9205@gmail.com>
2017-04-01 14:01:48 +0200

    - added library for handling ds18b20 sensors - added measure temperature from ds18b20 network - added passing temperature to ATCommandService method

--- RGBLight.ino
+++ RGBLight.ino
@@ -1,4 +1,9 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+
 #include "SuperfluxController.h"
+
 
 #define R 5
 #define G 3
@@ -12,6 +17,9 @@ bool checkIfStringReceive = false;
 
 SuperfluxControllerClass controller;
 unsigned long currentMills;
+OneWire oneWire(9);
+DallasTemperature sensors(&oneWire);
+
 
 void setup()
 {
@@ -20,6 +28,7 @@ void setup()
   Serial.begin(9600);
   controller.init(R,G,B);
   receiveString.reserve(256);
+  sensors.begin();
   
 
 }
@@ -29,12 +38,15 @@ void loop()
 
   /* add main program code here */
   
+  sensors.requestTemperatures();
+  float temperature = sensors.getTempCByIndex(0);
+  
   currentMills = millis();
   
   
   if (checkIfStringReceive){
 	  
-	  controller.ATCommandsService(receiveString,&controller.animation);
+	  controller.ATCommandsService(receiveString,&controller.animation,temperature);
 	  receiveString = "";
 	  checkIfStringReceive = false;
 	  
